6b07d391fe414be8b4789712b279e93f31effa68
shuxing.he <shuxing.he@percent.cn>
2022-08-19 14:06:17 +0800

    Site updated: 2022-08-19 14:06:16

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "10",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "120",
+    "top": "10",
+    "key": "e366c8d983a4427dbf3badad33772678"
+  }
+}
+
